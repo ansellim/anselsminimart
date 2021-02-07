2616de7ff036f0544fac8567d173fe2218218ef0
ansellim <ansellim@icloud.com>
2021-02-07 12:48:00 +0800

    updated website

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is my website
 # Updated
 
 have added cart functionality
+
+# updated
+
+cleaning up assignment for submission.
---
+++ javascript/feedback_form.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  document
+    .getElementById("feedback_form")
+    .addEventListener("submit", function () {
+      alert("Your form has been submitted");
+    });
+};
